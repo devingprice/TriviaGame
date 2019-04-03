fb2bdedccbbacf57569bb4723c8c7ee5afdaebce
Devin Price <devingprice@gmail.com>
2019-04-03 17:03:20 -0400

    Add border rope and bomb

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,3 @@
-/*
-Start screen with bomb in the middle and title 
-
-Click starts countdown from 3 
-
-Bomb moves to top left and rope fades in 
-
-Red line spins around count down timer  
-
-Then flys to left to ignite fire 
-
-Timer counts down inside of bomb 
-
-If all questions answered , hide fire and stop rope animation 
-
-If out of time, bomb explodes  
-
-Fire animation expands 
-
-Stops on screen 
-
-Red screen for loss  
-
-Bomb fades away on win 
-*/
 
 var qAKey = [
     {
@@ -58,7 +33,7 @@ function startAnim() {
 function displayClock() {
     console.log("clock")
     //var display = $('#timer');
-    startTimer( (timeGivenMS / 1000 ));
+    startTimer((timeGivenMS / 1000));
 }
 function addZeroIfSingleDigit(num) {
     if (num < 10) {
@@ -82,7 +57,7 @@ function startTimer(durationSec) {
         } else {
             minutes = addZeroIfSingleDigit(minutes);
             seconds = addZeroIfSingleDigit(seconds);
-    
+
             $('#timer').text(minutes + ":" + seconds);
         }
     }, 1000);
@@ -94,7 +69,8 @@ document.addEventListener('click', function () {
         startAnim();
         //after delay startQuiz()
         setTimeout(function () {
-            startQuiz()
+            lightFire();
+            startQuiz();
             console.log('started quiz');
         }, 1000)
     }
@@ -129,7 +105,7 @@ function displayRound() {
         )
     }
     $('.answer').on('click', function () {
-        if(!winLose){
+        if (!winLose) {
             randomSorted[currRound].chosen = $(this).attr('data-index');
             transitionQuestion();
         }
@@ -137,17 +113,17 @@ function displayRound() {
 }
 
 $('.answer').on('click', function () {
-    if(!winLose){
+    if (!winLose) {
         randomSorted[currRound].chosen = $(this).attr('data-index');
         transitionQuestion();
     }
 })
 function checkIfCorrect(qAItem) {
-    if(qAItem.chosen === qAItem.answer){
+    if (qAItem.chosen === qAItem.answer) {
         return true;
-    } 
+    }
     return false;
- }
+}
 function transitionQuestion() {
     console.log('trans')
     if ((currRound + 1) === numRounds) {
@@ -157,7 +133,7 @@ function transitionQuestion() {
         displayRound();
     }
 }
-function win(){
+function win() {
     winLose = 'win'
     clearInterval(displayTimer)
     clearTimeout(bombTimer)
@@ -171,34 +147,34 @@ function displayResultsKey() {
     // div class results + win class or lose class
     $('.answer').hide()
     $('.question').hide()
-    randomSorted.filter(function(qAItem){
+    randomSorted.filter(function (qAItem) {
         return qAItem.chosen !== undefined;
-    }).map(function(qAItem){
+    }).map(function (qAItem) {
         $('#results')
-            .append( $('<p>').text(qAItem.question ) )
-            .append( $('<p>').text(qAItem.answerList[qAItem.chosen] ) )
-            .append( $('<p>').text(qAItem.explaination ) )
-        
-        if( checkIfCorrect(qAItem) ){
-            $('#results').append( $('<p>').text("correct" ) )
+            .append($('<p>').text(qAItem.question))
+            .append($('<p>').text(qAItem.answerList[qAItem.chosen]))
+            .append($('<p>').text(qAItem.explaination))
+
+        if (checkIfCorrect(qAItem)) {
+            $('#results').append($('<p>').text("correct"))
         } else {
-            $('#results').append( $('<p>').text("incorrect" ) )
+            $('#results').append($('<p>').text("incorrect"))
         }
-        })
-    $('#results').append( 
-        $('<button>').text("Play Again" )
-        .on('click',function(){
-            playAgain()
-        })
+    })
+    $('#results').append(
+        $('<button>').text("Play Again")
+            .on('click', function () {
+                playAgain()
+            })
     )
     console.log(randomSorted)
-} 
-function playAgain() { 
+}
+function playAgain() {
     $('.answer').show().empty();
     $('.question').show().empty();
     $('#timer').fadeOut(0).text("00:05").fadeIn();
     $('#results').empty();
-    
+
     $('#press').fadeIn();
 
     started = false;
@@ -211,4 +187,45 @@ function playAgain() {
 
 
 
-$('#temp').append( $(window).width() )
+$('#temp').append($(window).width())
+$('#temp').append($(window).height())
+
+function lightFire(){
+    var rope = document.getElementById('rope-border');
+    var ropeEnd = document.getElementById('rope-end');
+
+    rope.classList.remove("progress-rope");
+    ropeEnd.classList.remove("lit");
+
+    void rope.offsetWidth;
+    void ropeEnd.offsetWidth;
+
+    rope.classList.add("progress-rope");
+    ropeEnd.classList.add("lit");
+}
+
+/*
+Start screen with bomb in the middle and title 
+
+Click starts countdown from 3 
+
+Bomb moves to top left and rope fades in 
+
+Red line spins around count down timer  
+
+Then flys to left to ignite fire 
+
+Timer counts down inside of bomb 
+
+If all questions answered , hide fire and stop rope animation 
+
+If out of time, bomb explodes  
+
+Fire animation expands 
+
+Stops on screen 
+
+Red screen for loss  
+
+Bomb fades away on win 
+*/
